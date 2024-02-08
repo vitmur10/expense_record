2205187c64fee85dd709f1fcc28d551c164e88d7
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2024-02-09 00:55:26 +0200

    requirement

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Const import *
 
 
 
-
+@dp.message_handler(commands=['start'])
+async def hello(message: aiogram.types.Message):
+    pass
 if __name__ == '__main__':
     aiogram.executor.start_polling(dp, skip_updates=True)
